433817cd51642135e4e9b7e6e574b3d5c913f4a1
MaximillianVoss <fosswolfgang@hotmail.com>
2023-07-19 22:15:00 +0300

    По правлен классификатор, но не находит шаблоны для отображения

---
+++ vectorizer.py
@@ -0,0 +1,11 @@
+from sklearn.feature_extraction.text import HashingVectorizer
+import os, pickle, re
+cur_dir = os.path.dirname(__file__)
+stop = pickle.load(open(os.path.join(cur_dir, 'stopwords.pkl'), 'rb'))
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+vect = HashingVectorizer(decode_error='ignore', n_features=2**21, preprocessor=None, tokenizer=tokenizer)
