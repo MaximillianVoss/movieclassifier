3130a3bbcf688eca8325f44c86a35c06e98997c9
MaximillianVoss <fosswolfgang@hotmail.com>
2023-07-19 22:22:52 +0300

    Удалены предыдущие мусорные приложения, ДО РЕФАТКОРИНГА КОДА И СТРУКТУРЫ!

--- vectorizer.py
+++ vectorizer.py
@@ -1,7 +1,7 @@
 from sklearn.feature_extraction.text import HashingVectorizer
 import os, pickle, re
 cur_dir = os.path.dirname(__file__)
-stop = pickle.load(open(os.path.join(cur_dir, 'stopwords.pkl'), 'rb'))
+stop = pickle.load(open(os.path.join(cur_dir, 'pkl_objects/stopwords.pkl'), 'rb'))
 def tokenizer(text):
     text = re.sub('<[^>]*>', '', text)
     emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
